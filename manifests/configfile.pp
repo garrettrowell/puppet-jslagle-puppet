806553c65d6318af4e30a1a34a8c516c17e259b2
Jason Slagle <jslagle@gmail.com>
2012-12-06 13:56:31 -0500

    Moving example config file to into etc/puppet directory

--- manifests/configfile.pp
+++ manifests/configfile.pp
@@ -3,7 +3,7 @@ class puppet::configfile inherits puppet::params {
   @file { $puppet::params::config:
     ensure  => present,
     replace => false,
-    source  => 'puppet:///modules/puppet/puppet.conf',
+    source  => 'puppet:///modules/puppet/etc/puppet/puppet.conf',
     owner   => 'root',
     mode    => '0644',
   }
